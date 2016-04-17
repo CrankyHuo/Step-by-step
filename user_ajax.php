2dc1a389376a9c11cf658771d10bccd0e9864f52
Tamamoran <liuyejunnan@163.com>
2016-04-17 13:33:29 +0800

    modified

--- user_ajax.php
+++ user_ajax.php
@@ -1,12 +1,12 @@
 <?php
     $username = $_GET['username'];
-    if($username === 'Yamato'){
-        $ret = array('sex'=>'female','location'=>'Kure Kaigun Kosho');
-    }else if($username === 'Taiho'){
-        $ret = array('sex'=>'female','location'=>'Kobe Kawasaki factory');
-    }else if($username === 'Tirpitz'){
-        $ret = array('sex'=>'female','location'=>'Kriegsmarinewerft Wilhelmshaven');
-    }else if($username === 'Enterprise'){
-        $ret = array('sex'=>'female','location'=>'Newport News Shipbuilding and Drydock Company');
+    if($username === 'yamato'){
+        $ret = array('sex'=>'female','home'=>'Kure Kaigun Kosho');
+    }else if($username === 'taiho'){
+        $ret = array('sex'=>'female','home'=>'Kobe Kawasaki factory');
+    }else if($username === 'tirpitz'){
+        $ret = array('sex'=>'female','home'=>'Kriegsmarinewerft Wilhelmshaven');
+    }else if($username === 'enterprise'){
+        $ret = array('sex'=>'female','home'=>'Newport News Shipbuilding and Drydock Company');
     }
     echo json_encode($ret);
